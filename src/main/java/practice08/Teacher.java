b5af7f148dfb648b19dfa3e9a4e7b0b53db24413
Liufan666 <1663531467@qq.com>
2020-07-22 22:21:16 +0800

    [OO Step by Step] [Kevin] feature: finished practice08

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,42 @@
 package practice08;
 
-public class Teacher {
+
+public class Teacher extends Person{
+    protected Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String str = super.introduce();
+        if (klass==null){
+            str += " I am a Teacher. I teach No Class.";
+        }
+        else{
+            str += " I am a Teacher. I teach Class "+klass.getNumber()+".";
+        }
+        return str;
+    }
+    public String introduceWith(Student student){
+        String str = super.introduce();
+        if(this.klass.getNumber()==student.klass.getNumber()){
+            str += " I am a Teacher. I teach "+student.getName()+".";
+        }
+        else {
+            str += " I am a Teacher. I don't teach "+student.getName()+".";
+        }
+        return str;
+
+    }
 }

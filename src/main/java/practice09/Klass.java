c28b206334226790ebf3fd675297417350411d53
Liufan666 <1663531467@qq.com>
2020-07-22 23:18:02 +0800

    [OO Step by Step] [Kevin] feature: finished practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,40 @@
 package practice09;
 
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private int joinId;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+    public void assignLeader(Student student){
+        if(this.joinId!=student.getId()){
+            System.out.print("It is not one of us.\n");
+        }
+        else {
+            this.leader = student;
+        }
+
+    }
+
+    public String getDisplayName() {
+        return "Class "+this.number;
+
+    }
+
+    public void appendMember(Student student) {
+//        System.out.println(student.getId());
+        this.joinId = student.getId();
+
+    }
 }
--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,46 @@
 package practice09;
 
-public class Person {
+import java.util.Objects;
+
+public class Person  {
+    protected int id;
+    protected String name;
+    protected int age;
+
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String introduce(){
+        String str = "My name is "+name+". I am "+age+" years old.";
+        return str;
+    }
 }

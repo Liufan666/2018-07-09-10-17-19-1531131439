13466e112d3b11dc2ccee1100923f65daf096b8c
Liufan666 <1663531467@qq.com>
2020-07-22 16:55:44 +0800

    [OO Step by Step] [Kevin] feature: finished practice01 and practice02

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,25 @@
 package practice02;
 
 public class Person {
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String introduce(){
+
+        String str = "My name is "+name+". I am "+age+" years old.";
+        return str;
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,19 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    protected int klass;
+    @Override
+    public String introduce(){
+        String str = "I am a Student. I am at Class "+klass+".";
+        return str;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
 }
